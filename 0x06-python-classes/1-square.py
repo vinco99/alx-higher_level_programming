56ba6800de0f527af79d23c372736d01cfa72f1e
vinco99 <nwikeduff@gmail.com>
2023-02-23 20:28:31 +0100

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+
 """
 1. A class Square that accepts a size for the square
     contains a funtion that initialize the size
 """
+
 class Square:
     """
     A Square class that accepts size
     """
+
     def __init__(self, size):
         """
         initializes the size
         """
-        self.__size =
+        self.__size = size
