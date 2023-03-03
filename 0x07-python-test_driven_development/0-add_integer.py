18f4cf1884044dec953deea0b95e77cb30c136ff
Nwike Vincent Ikechukwu <nwikeikechukwu@gmaiil.com>
2023-03-03 13:02:25 +0000

    solving tasks

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,14 +3,16 @@
 This is a 0_add_integer module
 It has a funtion def add_integer(a, b=98):
 """
+
+
 def add_integer(a, b=98):
-	"""Return the sum of two numbers a and b"""
-	if not isinstance(a, (int, float)):
-		raise TypeError("a must be an integer")
-	if not isinstance(b, (int, float)):
-		raise TypeError("b must be an integer")
-	if type(a) is float:
-		a = int(a)
-	if type(b) is float:
-		b = int(b)
-	return a + b
+    """Return the sum of two numbers a and b"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
